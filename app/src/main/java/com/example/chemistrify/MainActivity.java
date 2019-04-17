e9a228587930f7bac3f5cacdbfb806cdfbc67852
Gerardo Vázquez <gerardolgvr0310@gmail.com>
2019-04-16 23:38:37 -0500

    Agregando ElementActivity, fuentes y colores

--- app/src/main/java/com/example/chemistrify/MainActivity.java
+++ app/src/main/java/com/example/chemistrify/MainActivity.java
@@ -1,13 +1,29 @@
 package com.example.chemistrify;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    Button btnElemento;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnElemento = (Button) findViewById(R.id.button);
+
+        btnElemento.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), ElementActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
