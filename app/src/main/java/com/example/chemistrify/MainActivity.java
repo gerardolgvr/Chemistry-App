d7dbb8205d6f6d69e86e0ec39da4c999bee6cdb6
Gerardo Vázquez <gerardolgvr0310@gmail.com>
2019-04-18 19:23:01 -0500

    Agregando menu en MainActivity

--- app/src/main/java/com/example/chemistrify/MainActivity.java
+++ app/src/main/java/com/example/chemistrify/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.chemistrify;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -34,5 +37,31 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.app_bar_search:
+                //open search activity
+                openSearchActivity();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    //function that opens SearchActivity
+    private void openSearchActivity(){
+        Intent intent = new Intent(getApplicationContext(), SearchActivity.class);
+        startActivity(intent);
     }
 }
